c6a7b100cea9a7f72cf5653cab8a0ff8d89cf0b1
SergioCrisostomo <sergiosbox@gmail.com>
2019-12-27 17:09:55 +0100

    Add engagement view

---
+++ src/components/EngagementOverview.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2>Engagement overview</h2>
+    <table data-type="engagement">
+      <thead>
+        <tr>
+          <th>{{ staffLabel }}</th>
+          <th v-for="mark in timeMarks.marks" :key="mark">
+            {{ mark | formatDate }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(person, rowIndex) in engagementPerPerson"
+          :key="person.id + '_' + rowIndex"
+          :data-staff="person.id"
+        >
+          <th>{{ person.name }}</th>
+          <td v-for="(engagement, i) in person.engagements" :key="i">
+            {{ engagement }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EngagementOverview",
+  props: ["tasksPerPerson", "timeMarks", "staffLabel"], // TODO: add types and validators
+
+  computed: {
+    engagementPerPerson() {
+      return this.tasksPerPerson.map(person => {
+        return {
+          name: person.name,
+          engagements: this.timeMarks.marks.map(timestamp => {
+            return person.tasks.filter(Boolean).reduce((sum, task) => {
+              if (task === null) return sum;
+              const start = task.start.getTime();
+              const end = task.end.getTime();
+
+              if (start <= timestamp && timestamp <= end)
+                return sum + task.engagement;
+              return sum;
+            }, 0);
+          })
+        };
+      });
+    }
+  },
+  filters: {
+    formatDate(date) {
+      if (typeof date === "number") date = new Date(date);
+      return [date.getDate(), date.getMonth() + 1]
+        .map(nr => (nr > 9 ? nr : "0" + nr))
+        .join("/");
+    }
+  }
+};
+</script>
+
+<style>
+table[data-type="engagement"] td {
+  text-align: center;
+}
+</style>
